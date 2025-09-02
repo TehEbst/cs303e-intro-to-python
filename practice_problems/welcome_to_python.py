052ae18b687602e5ba05ac38049db764567bd09c
Brayden Medina <medinabrayden@utexas.edu>
2025-09-02 16:29:42 -0500

    feat(welcome_to_python.py): add a practice problem and solution

---
+++ practice_problems/welcome_to_python.py
@@ -0,0 +1,33 @@
+'''
+Write a program that displays "Welcome to Python" five times.
+
+Input Format
+
+There is no input.
+
+Constraints
+
+N/A
+
+Output Format
+
+The string "Welcome to Python" should be displayed on each of the first five lines.
+
+Sample Output 0
+
+Welcome to Python
+Welcome to Python
+Welcome to Python
+Welcome to Python
+Welcome to Python
+Explanation 0
+
+The string "Welcome to Python" is printed five times.
+'''
+
+# define the line that we want to print out
+line = "Welcome to Python"
+
+# i starts as 0 and iterates up to 4 for a range of 5
+for i in range(5):
+    print(line)
