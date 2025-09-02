c9d3a59820f979a519c5b016678a55f0f9164483
Brayden Medina <medinabrayden@utexas.edu>
2025-09-02 17:07:21 -0500

    feat(simple_finite_series_summation.py): add a practice problem and solution

---
+++ practice_problems/simple_finite_series_summation.py
@@ -0,0 +1,25 @@
+'''
+Write a program that displays the result of 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+
+Input Format
+
+There is no input.
+
+Constraints
+
+N/A
+
+Output Format
+
+The sum of the series should be displayed.
+
+Sample Output 0
+
+45
+Explanation 0
+
+The sum of the series is printed.
+'''
+
+# Print out the summation of integers
+print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
