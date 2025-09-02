b72349180dcb87e969becce56872b37665322bcd
Brayden Medina <medinabrayden@utexas.edu>
2025-09-02 17:06:44 -0500

    feat(compute_simple_expression.py): add a practice problem and solution

---
+++ practice_problems/compute_simple_expression.py
@@ -0,0 +1,27 @@
+'''
+Write a program that displays the result of
+
+(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+
+Input Format
+
+There is no input.
+
+Constraints
+
+N/A
+
+Output Format
+
+The result of the expression should be displayed.
+
+Sample Output 0
+
+0.8392857142857143
+Explanation 0
+
+The result of the expression is printed.
+'''
+
+# Compute the equation
+print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5))
