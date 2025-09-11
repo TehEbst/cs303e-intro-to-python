b1d57818c2f70fd20fc65dc308de47225191bc12
Brayden Medina <medinabrayden@utexas.edu>
2025-09-10 20:49:53 -0500

    feat(Quiz0.py): add the completed solution to quiz 0

---
+++ quizzes_exams/Quiz0.py
@@ -0,0 +1,41 @@
+# CS 303E Quiz 0
+# do NOT rename this file, otherwise Gradescope will not accept your submission
+
+
+# Problem 1: Say Hello
+def sayHello():
+    # have user input names
+    friend_one = input()
+    friend_two = input()
+
+    # print out each greeting used an f string
+    print(f'Hello {friend_one}!')
+    print(f'Hello {friend_two}!')
+    pass
+
+
+# Problem 2: Sum Three Numbers
+def sumThreeNumbers():
+    # have user input three numbers
+    num_one = int(input())
+    num_two = int(input())
+    num_three = int(input())
+
+    # print sum of three numbers
+    print(num_one + num_two + num_three)
+    pass
+
+
+if __name__=="__main__":
+    """
+    If you want to test your code on your computer, uncomment the respective
+    function call(s) below.
+    
+    DO NOT CALL YOUR FUNCTIONS ANYWHERE OUTSIDE OF THIS AREA
+    """
+    # sayHello()
+    # sumThreeNumbers()
+
+    # DO NOT DELETE THIS PASS
+    pass
+    # DON'T DO IT
