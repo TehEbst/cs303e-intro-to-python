abd24444d17692f56cf849c466a458923bc34485
Brayden Medina <medinabrayden@utexas.edu>
2025-09-17 14:49:05 -0500

    fix(two_sum.py): add more efficient method of computation

--- practice_problems/Week03/two_sum.py
+++ practice_problems/Week03/two_sum.py
@@ -1,81 +1,33 @@
-import matplotlib.pyplot as plt
-from random import randint
-
 class Solution:
-
 
     def __init__(self, nums, target):
         self.nums = nums
         self.target = target
 
     def twoSum(self):
-        limit = len(self.nums)
-        lb = [0]
-        ub = [limit - 1]
-        lb_dir = 1
-        ub_dir = 1
-        sum = []
-        for i in range(10):
-            print()
-            print(f'loop {i+1}')
-            print(f'lb: {lb[-1]}, value: {self.nums[lb[-1]]}')
-            print(f'ub: {ub[-1]}, value: {self.nums[ub[-1]]}')
+        my_dict = {}
+        ind = 0
+        my_answer = []
+        for num in self.nums:
+            if num in my_dict:
+                my_dict[num].append(ind)
+            else:
+                my_dict[num] = [ind]
 
-            sum.append(self.nums[lb[-1]] + self.nums[ub[-1]])
+            if self.target - num in my_dict and my_dict[num] != my_dict[self.target - num]:
+                my_answer.append(my_dict[num][0])
+                my_answer.append(my_dict[self.target - num][0])
 
-            if sum[-1] == self.target and lb[-1] != ub[-1]:
-                break
+            ind += 1
 
-            if lb[-1] == 0:
-                lb_dir = 1
-            elif lb[-1] == limit - 1:
-                lb_dir = -1
+        if self.target % 2 == 0 and len(my_answer ) == 0:
+            my_answer = my_dict[self.target / 2]
+        return sorted(my_answer)
 
-            if ub[-1] == 0:
-                ub_dir = 1
-            elif ub[-1] == limit - 1:
-                ub_dir = -1
 
-            if abs(self.nums[lb[-1]]) > self.target:
-                lb.append(lb[-1] + lb_dir)
-                ub.append(ub[-1])
-                print('move lb by 1')
-                print(f'lb: {lb[-1]}, value: {self.nums[lb[-1]]}')
-                print(f'ub: {ub[-1]}, value: {self.nums[ub[-1]]}')
-            elif abs(self.nums[lb[-1]] + self.nums[ub[-1]]) > abs(self.target):
-                ub.append(ub[-1] + ub_dir)
-                lb.append(lb[-1])
-                print('move ub by 1')
-                print(f'lb: {lb[-1]}, value: {self.nums[lb[-1]]}')
-                print(f'ub: {ub[-1]}, value: {self.nums[ub[-1]]}')
-            elif abs(self.nums[lb[-1]] + self.nums[ub[-1]]) < abs(self.target):
-                lb.append(lb[-1] + lb_dir)
-                ub.append(ub[-1])
-                print('move lb by 1')
-                print(f'lb: {lb[-1]}, value: {self.nums[lb[-1]]}')
-                print(f'ub: {ub[-1]}, value: {self.nums[ub[-1]]}')
+my_input = [3, 3]
+my_target = 6
 
-        if not self.nums[lb[-1]] < self.nums[ub[-1]]:
-            print('flip')
-            temp = lb[-1]
-            lb.append(ub[-1])
-            ub.append(temp)
-            print(f'lb: {lb[-1]}, value: {self.nums[lb[-1]]}')
-            print(f'ub: {ub[-1]}, value: {self.nums[ub[-1]]}')
+my_Solution = Solution(my_input, my_target)
 
-        return lb, ub, sum
-
-vector = [150,24,79,50,88,345,3]
-targ = 200
-
-sol = Solution(nums=vector, target=targ)
-
-x_lb, x_ub, x_sum = (sol.twoSum())
-print(x_lb)
-print(x_ub)
-
-plt.plot(x_ub)
-plt.plot(x_lb)
-plt.plot(x_sum)
-plt.show()
-
+print( my_Solution.twoSum() )
